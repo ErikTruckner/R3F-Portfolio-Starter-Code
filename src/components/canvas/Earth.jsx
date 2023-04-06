8308d5a963f99ae7d5c4e52640300cf64c55000b
Erik Truckner <75428920+ErikTruckner@users.noreply.github.com>
2023-04-06 09:13:16 -0400

    Lesson 12 Final +

--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -1,45 +1,28 @@
-import { useEffect, useState, useCallback, useMemo } from 'react'
-import { useGLTF, useAnimations } from '@react-three/drei'
-
-const actionNames = ['Base Stack']
+import { useEffect, useState } from 'react'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
+import { useFrame } from '@react-three/fiber'
 
 const Earth = () => {
   const [scale, setScale] = useState(0.7)
 
-  const memoizedEarth = useMemo(() => {
-    return useGLTF('./earth/scene.gltf')
+  useEffect(() => {
+    const loader = new GLTFLoader()
+    loader.load('./earth/scene.gltf', (gltf) => {
+      gltf.scene.scale.set(scale, scale, scale)
+      gltf.scene.traverse((child) => {
+        if (child.isMesh) {
+          child.material.side = THREE.DoubleSide
+        }
+      })
+      scene.add(gltf.scene)
+    })
+  }, [scale])
+
+  useFrame(() => {
+    setScale(scale + 0.01)
   })
 
-  const animations = useAnimations(
-    memoizedEarth.animations,
-    memoizedEarth.scene
-  )
-
-  const onScroll = useCallback(() => {
-    const percentage =
-      window.scrollY / (document.body.scrollHeight - window.innerHeight)
-    const newScale = 0.7 + 3 * percentage // adjust the scaling factor as desired
-    setScale(newScale)
-  }, [])
-
-  useEffect(() => {
-    actionNames.forEach((actionName) => {
-      const action = animations.actions[actionName]
-      action.play()
-    })
-
-    window.addEventListener('scroll', onScroll)
-
-    return () => {
-      window.removeEventListener('scroll', onScroll)
-    }
-  }, [animations, onScroll])
-
-  return (
-    <mesh>
-      <primitive object={memoizedEarth.scene} scale={scale} />
-    </mesh>
-  )
+  return <mesh />
 }
 
 export default Earth
